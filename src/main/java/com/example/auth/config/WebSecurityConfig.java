88cf4d882cc62c2d3a4e2c5dff3f9ee0c6872cd0
edujeeho <edujeeho@gmail.com>
2024-08-26 11:03:49 +0900

    form login

--- src/main/java/com/example/auth/config/WebSecurityConfig.java
+++ src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -3,6 +3,13 @@ package com.example.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,11 +18,53 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http
     ) throws Exception {
-        http.authorizeHttpRequests(
-                auth -> auth.requestMatchers("/no-auth")
+        // URL에 따라 접근 권한을 조정하는 방법
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/no-auth", "/")
+                            .permitAll();
+                    // 인증이 된 사용자만 허용하는 URL
+                    auth.requestMatchers("/users/my-profile")
+                            .authenticated();
+//                    auth.requestMatchers("/")
+//                            .permitAll();
+//                    auth.requestMatchers("/not-login-only")
+//                            .anonymous();
+//                    auth.requestMatchers("/nobody")
+//                            .denyAll();
+                })
+                // 로그인은 어떤 URL에서 어떤 방식으로 이뤄지는지
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/users/login")
+                        .defaultSuccessUrl("/users/my-profile")
+                        .failureUrl("/users/login?fail")
                         .permitAll()
-        );
+                );
+
+        // before 6.1
+        /*
+        http.authorizeHttpRequests()
+                .requestMatchers("/no-auth")
+                .permitAll()
+                .and();
+         */
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(
+            PasswordEncoder passwordEncoder
+    ) {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder.encode("password"))
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

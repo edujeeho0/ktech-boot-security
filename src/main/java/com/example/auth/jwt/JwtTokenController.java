b14961f9a695c2d5c81f061a88547eb80e54c75e
edujeeho <edujeeho@gmail.com>
2024-08-28 09:50:15 +0900

    issue jwt

--- src/main/java/com/example/auth/jwt/JwtTokenController.java
+++ src/main/java/com/example/auth/jwt/JwtTokenController.java
@@ -1,30 +1,72 @@
 package com.example.auth.jwt;
 
+import com.example.auth.jwt.dto.JwtRequestDto;
+import com.example.auth.jwt.dto.JwtResponseDto;
 import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
 @RequestMapping("/token")
 public class JwtTokenController {
     private final JwtTokenUtils tokenUtils;
+    // 1. 사용자 정보를 조회하는 방법
+    private final UserDetailsService userService;
+    // 2. 비밀번호를 대조하는 방법
+    private final PasswordEncoder passwordEncoder;
+
     public JwtTokenController(
-            JwtTokenUtils tokenUtils
+            JwtTokenUtils tokenUtils,
+            UserDetailsService userService,
+            PasswordEncoder passwordEncoder
     ) {
         this.tokenUtils = tokenUtils;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/test")
-    public String testIssueJwt() {
-        return tokenUtils.generateToken(User
-                .withUsername("alex")
-                .password("adsf")
-                .build());
+    @PostMapping("/issue")
+    public JwtResponseDto issueJwt(
+            @RequestBody
+            JwtRequestDto dto
+    ) {
+        UserDetails userDetails;
+        // 사용자 정보를 조회하자.
+        try {
+             userDetails = userService.loadUserByUsername(dto.getUsername());
+        } catch (UsernameNotFoundException ignored) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        // 비밀번호를 대조하자.
+        if (!passwordEncoder.matches(
+                dto.getPassword(), userDetails.getPassword()))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+
+        // JWT 발급
+        String jwt = tokenUtils.generateToken(userDetails);
+        JwtResponseDto response = new JwtResponseDto();
+        response.setToken(jwt);
+        return response;
     }
 
+//    @GetMapping("/generate-test")
+//    public String testIssueJwt() {
+//        return tokenUtils.generateToken(User
+//                .withUsername("alex")
+//                .password("adsf")
+//                .build());
+//    }
+//
     @GetMapping("/validate-test")
     public String validateTest(
             @RequestParam("token")
@@ -33,15 +75,5 @@ public class JwtTokenController {
         if (!tokenUtils.validate(token))
             return "not valid jwt";
         return "valid jwt";
-    }
-
-    @GetMapping("/validate-header")
-    public Claims validateHeader(
-            @RequestHeader(HttpHeaders.AUTHORIZATION)
-            String authorizationHeader
-    ) {
-        log.info(authorizationHeader);
-        String token = authorizationHeader.split(" ")[1];
-        return tokenUtils.parseClaims(token);
     }
 }
